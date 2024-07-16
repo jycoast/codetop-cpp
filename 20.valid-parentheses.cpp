5e141847445e992191b925f27c1f8c06054efa1c
jiyongchao <jycoder@163.com>
2024-07-16 16:16:22 +0800

    添加题目

--- 20.valid-parentheses.cpp
+++ 20.valid-parentheses.cpp
@@ -10,8 +10,23 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-    bool isValid(string s) {
-        
+  bool isValid(string s) {
+    string stk; // 用字符串来模拟栈
+    for (char c : s) {
+      if (c == '(' || c == '[' || c == '{') {
+        stk.push_back(c);
+      } else if (stk.empty() || !isMath(stk.back(), c)) {
+        return false;
+      } else {
+        stk.pop_back();
+      }
     }
+    return stk.empty();
+  }
+
+  bool isMath(char l, char r) {
+    return (l == '(' && r == ')') || (l == '[' && r == ']') ||
+           (l == '{' && r == '}');
+  }
 };
 // @lc code=end
